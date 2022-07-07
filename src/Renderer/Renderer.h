9c4ca0b059a0b6c11621b674b5ca31ca1f3b48a2
olivierfradette-roy <olivier.fradette.roy@gmail.com>
2022-07-07 10:50:14 -0400

    move-scale-cube

--- src/Renderer/Renderer.h
+++
@@ -1,6 +0,0 @@
-#include "GL/glew.h"
-#include "../TranslateMatrix/TranslateMatrix.h"
-
-class Renderer {
-    static void setTranslationMatrix(int *shaderProgram, TranslateMatrix translateMatrix);
-};
