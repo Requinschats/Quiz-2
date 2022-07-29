baf9d36920652be43f3209661cb556b01a64077b
olivierfradette-roy <olivier.fradette.roy@gmail.com>
2022-07-28 20:55:20 -0400

    comments

--- src/initialization/initialization.cpp
+++ src/initialization/initialization.cpp
@@ -14,12 +14,15 @@ void window_size_callback(GLFWwindow *window, int width, int height) {
 GLFWwindow *initializeWindow() {
     glfwInit();
 
+    // Set all the required options for GLFW including versions and profile
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
+    // Create a GLFWwindow object that we can use for GLFW's functions
     GLFWwindow *window = glfwCreateWindow(1024, 768, "Comp371 - Assignment 1", NULL, NULL);
+    //error handling
     if (window == NULL) {
         std::cerr << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
